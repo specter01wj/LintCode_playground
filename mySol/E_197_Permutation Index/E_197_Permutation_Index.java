967d5ff05bcb91d446281d89751ec05f3b0fc7ef
specter01wj <specter01wj@gmail.com>
2018-02-23 17:22:24 -0500

    02/23/2018 v3

---
+++ mySol/E_197_Permutation Index/E_197_Permutation_Index.java
@@ -0,0 +1,41 @@
+/*
+Given a permutation which contains no repeated number, find its index in all the permutations of these numbers, which are ordered in lexicographical order. The index begins at 1.
+
+Example
+Given [1,2,4], return 1.
+*/
+
+import java.util.*;
+
+public class E_197_Permutation_Index {
+
+    public static void main(String[] args) {
+
+        int[] input = {1,2,3,4,5,6};
+        long output = permutationIndex(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + output);
+
+    }
+
+    /*
+     * @param A: An array of integers
+     * @return: A long integer
+     */
+    public static long permutationIndex(int[] A) {
+        // write your code here
+        long index = 1, fact = 1;
+
+        for (int i = A.length - 1; i >= 0; i--) {
+            int rank = 0;
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] > A[j]) {
+                    rank++;
+                }
+            }
+            index += rank * fact;
+            fact *= A.length - i;
+        }
+        return index;
+    }
+
+}
