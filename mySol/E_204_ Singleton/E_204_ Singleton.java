62ca6e994b82d4f8d3b903c9cc271574bdc18a5b
specter01wj <specter01wj@gmail.com>
2018-02-24 20:57:40 -0500

    02/24/2018 v1

---
+++ mySol/E_204_ Singleton/E_204_ Singleton.java
@@ -0,0 +1,38 @@
+/*
+Singleton is a most widely used design pattern. If a class has and only has one instance at every moment, we call this design as singleton. For example, for class Mouse (not a animal mouse), we should design it in singleton.
+
+You job is to implement a getInstance method for given class, return the same instance of this class every time you call this method.
+
+Example
+In Java:
+
+A a = A.getInstance();
+A b = A.getInstance();
+a should equal to b.
+*/
+
+import java.util.*;
+
+public class E_204_ Singleton {
+
+    public static void main(String[] args) {
+
+        Solution a = Solution.getInstance();
+        Solution b = Solution.getInstance();
+
+    }
+
+    /**
+     * @return: The same instance of this class every time
+     */
+    private static Solution solution = new Solution();
+
+    private Solution() {
+    }
+    
+    public static Solution getInstance() {
+        // write your code here
+        return solution;
+    }
+
+}
