d1b6ad3779452ae4b19b836241be04722b3e812a
specter01wj <specter01wj@gmail.com>
2018-03-12 00:10:11 -0400

    03/11/2018 v1

---
+++ mySol/E_451_Swap Nodes in Pairs/E_451_Swap Nodes in Pairs.java
@@ -0,0 +1,44 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+Example
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+import java.util.*;
+
+public class E_375_Clone_Binary_Tree {
+
+    public static void main(String[] args) {
+
+        int[] input = {1,2,3,4};
+        ListNode output = swapPairs(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + output);
+
+    }
+
+    /*
+     * @param head: a ListNode
+     * @return: a ListNode
+     */
+    public static ListNode swapPairs(ListNode head) {
+        // write your code here
+        if (head == null) return null;
+        if (head.next == null) return head;
+        head.next.next = swapPairs(head.next.next);
+        ListNode tmp = head.next;
+        head.next = tmp.next;
+        tmp.next = head;
+        return tmp;
+    }
+
+}
