498901cf6c36ecfd5a848ba39f6495f6ebaa192c
specter01wj <specter01wj@gmail.com>
2018-03-26 01:27:35 -0400

    03/25/2018 v1

---
+++ mySol/E_547_Intersection of Two Arrays/E_547_Intersection_of_Two_Arrays.java
@@ -0,0 +1,53 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+ Notice
+Each element in the result must be unique.
+The result can be in any order.
+
+Example
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+*/
+
+import java.util.*;
+
+public class E_547_Intersection_of_Two_Arrays {
+
+    public static void main(String[] args) {
+
+        int[] input1 = {1, 2, 3, 4, 2, 1};
+        int[] input2 = {2, 3, 8};
+        int[] output = intersection(input1, input2);
+        System.out.println("input1: " + Arrays.toString(input1) + 
+            " input2: " + Arrays.toString(input2) + "\noutput: " + Arrays.toString(output));
+
+    }
+
+    /*
+     * @param nums1: an integer array
+     * @param nums2: an integer array
+     * @return: an integer array
+     */
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        // write your code here
+        Set<Integer> set1 = new HashSet<>();
+        for (int i : nums1) set1.add(i);
+
+        Set<Integer> result = new HashSet<>();
+        for (int i : nums2) {
+            if (set1.contains(i)) {
+                result.add(i);
+            }
+        }
+        ;
+
+        int[] arr = new int[result.size()];
+        int k = 0;
+        for (int i : result) {
+            arr[k++] = i;
+        }
+
+        return arr;
+    }
+
+}
